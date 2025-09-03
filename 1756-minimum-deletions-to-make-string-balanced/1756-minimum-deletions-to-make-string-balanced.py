91ae6da76ef083a02705b6de5754274b6ad7d726
Rajnish Maurya <44161591+Rajnishmaurya@users.noreply.github.com>
2025-09-03 10:08:54 +0530

    Time: 183 ms (100%), Space: 13 MB (91.18%) - LeetHub

---
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def minimumDeletions(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        count,result=0,0
+
+        for c in s:
+            if c=='b':
+                count+=1
+            elif count:
+                result+=1
+                count-=1
+        return result
+        
