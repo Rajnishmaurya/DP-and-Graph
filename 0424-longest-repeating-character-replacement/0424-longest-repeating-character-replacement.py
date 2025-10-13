c03dc41ae58efa67503d1989de2061748884d635
Rajnish Maurya <44161591+Rajnishmaurya@users.noreply.github.com>
2025-10-13 10:01:00 +0530

    Time: 111 ms (84.12%), Space: 18 MB (49.42%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,22 +1,19 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        left=0
         lis=[0]*26
-        l=0
-        r=0
         max_len=0
         max_freq=0
 
-        for r in range(len(s)):
-            temp=ord(s[r])-ord('A')
+        for right in range(len(s)):
+            temp=ord(s[right])-ord('A')
             lis[temp]+=1
+
             max_freq=max(max_freq,lis[temp])
-            if r-l+1-max_freq>k:
-                index=ord(s[l])-ord('A')
+
+            if right-left+1-max_freq>k:
+                index=ord(s[left])-ord('A')
                 lis[index]-=1
-                l+=1
-
-            max_len=max(max_len,r-l+1)
-        return max_len
-
-            
-        
+                left+=1
+            max_len=max(max_len,right-left+1)
+        return max_len        
