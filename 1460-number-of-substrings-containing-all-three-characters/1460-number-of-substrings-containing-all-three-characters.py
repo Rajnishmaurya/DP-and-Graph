8b50e5c1146662283391e49a913bdea237f581be
Rajnish Maurya <44161591+Rajnishmaurya@users.noreply.github.com>
2025-10-13 11:24:13 +0530

    Time: 71 ms (91.67%), Space: 17.8 MB (65%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.py
@@ -3,4 +3,8 @@ class Solution:
         lastseen=[-1]*3
         count=0
         for i in range(len(s)):
-            lastseen[ord(s[i])-ord()]        
+            lastseen[ord(s[i])-ord('a')]=i
+
+            if lastseen[0]!=-1 and lastseen[1]!=-1 and lastseen[2]!=-1:
+                count+=1+min(lastseen)     
+        return count   
