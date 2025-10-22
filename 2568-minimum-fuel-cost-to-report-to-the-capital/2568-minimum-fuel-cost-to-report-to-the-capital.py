dd2d0b273bfd612bd1fddb67ad9393b2ee86de85
Rajnish Maurya <44161591+Rajnishmaurya@users.noreply.github.com>
2025-10-22 17:21:31 +0530

    Time: 254 ms (21.99%), Space: 58.4 MB (97.25%) - LeetHub

--- 2568-minimum-fuel-cost-to-report-to-the-capital/2568-minimum-fuel-cost-to-report-to-the-capital.py
+++ 2568-minimum-fuel-cost-to-report-to-the-capital/2568-minimum-fuel-cost-to-report-to-the-capital.py
@@ -1,33 +1,36 @@
 class Solution:
     def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
         adj_list=defaultdict(list)
-        n=len(roads)+1
-        degree=[0]*n
+        n=len(roads)
+        indegree=[0]*(n+1)
+
         people=[1]*(n+1)
 
-        for x,y  in roads:
+        for x,y in roads:
             adj_list[x].append(y)
             adj_list[y].append(x)
-            degree[x]+=1
-            degree[y]+=1
+            indegree[x]+=1
+            indegree[y]+=1
         
         q=deque()
-        for i in range(1,n):
-            if degree[i]==1:
+
+        for i in range(1,n+1):
+            if indegree[i]==1:
                 q.append(i)
             
-        answer=0
+        ans=0
 
         while q:
             temp=q.popleft()
-            answer+=ceil(people[temp]/seats)
-            for neighbour in adj_list[temp]:
-                degree[neighbour]-=1
-                people[neighbour]+=people[temp]
-                if degree[neighbour]==1 and neighbour!=0:
-                    q.append(neighbour)
-        return answer
+            ans+=ceil(people[temp]/seats)
+
+            for node in adj_list[temp]:
+                indegree[node]-=1
+                people[node]+=people[temp]
+                if indegree[node]==1 and node!=0:
+                    q.append(node)
+        return ans
 
 
-
+        
         
